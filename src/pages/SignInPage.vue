c80c3ce3f8ca0bd0bb429330fe04366c47da6ec4
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2025-07-03 19:50:37 +0300

    recactor: error message text and styles, btn loader added

--- src/pages/SignInPage.vue
+++ src/pages/SignInPage.vue
@@ -15,12 +15,20 @@ const password = ref('')
 const rememberMe = ref(false)
 const errorMessage = ref('')
 
+const isLoading = ref(false)
+
 const handleLogin = async () => {
+  if (isLoading.value) return
+  isLoading.value = true
+  errorMessage.value = ''
+
   try {
     await loginUser(email.value, password.value, rememberMe.value)
     router.push({ name: ROUTE_NAMES.LIST })
   } catch (error) {
     errorMessage.value = 'Login failed. Please check your email and password.'
+  } finally {
+    isLoading.value = false
   }
 }
 </script>
@@ -48,12 +56,20 @@ const handleLogin = async () => {
         </router-link>
       </div>
 
-      <p v-if="errorMessage" class="text-red-500 text-sm mb-4">{{ errorMessage }}</p>
-
       <div class="flex gap-6">
-        <CustomButton type="filled" @click="handleLogin">Login</CustomButton>
+        <CustomButton type="filled" @click="handleLogin" :disabled="isLoading">
+          <template v-if="isLoading">Logging in...</template>
+          <template v-else>Login</template>
+        </CustomButton>
         <CustomButton type="outlined" :to="{ name: ROUTE_NAMES.SIGN_UP }">Sign Up</CustomButton>
       </div>
+
+      <p
+        class="text-red-500 text-sm mt-4 min-h-[20px] transition-opacity duration-200"
+        :class="{ 'opacity-0': !errorMessage, 'opacity-100': errorMessage }"
+      >
+        {{ errorMessage || '' }}
+      </p>
     </form>
   </AuthLayout>
 </template>
